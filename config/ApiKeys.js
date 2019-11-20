b08dd9909bffa3aa6ce845ca83db89958481f559
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-11-20 22:48:57 +0500

    Update ApiKeys.js

--- config/ApiKeys.js
+++ config/ApiKeys.js
@@ -1,12 +1,12 @@
 export default {
     firebaseConfig: {
-        apiKey: "AIzaSyBbJYj0Ua0vnpX0W0E90p-13s64LALJGN8",
-        authDomain: "kfcapp-6bd76.firebaseapp.com",
-        databaseURL: "https://kfcapp-6bd76.firebaseio.com",
-        projectId: "kfcapp-6bd76",
-        storageBucket: "kfcapp-6bd76.appspot.com",
-        messagingSenderId: "1012767621218",
-        appId: "1:1012767621218:web:f4bc315c01677a84c5d13b",
-        measurementId: "G-LNKD19EY0Y"
+        apiKey: "*************",
+        authDomain: "**********",
+        databaseURL: "*********",
+        projectId: "**********",
+        storageBucket: "*********",
+        messagingSenderId: "*************",
+        appId: "***************",
+        measurementId: "***************"
     }
 }
